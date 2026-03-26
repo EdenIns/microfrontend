e0bf3f1f5595f329027171fa9b906564ab5d53b2
EdenIns <e.oudaya@outlook.com>
2026-03-26 14:55:46 +0100

    do todo

--- mfe-cart/src/components/Cart.jsx
+++ mfe-cart/src/components/Cart.jsx
@@ -7,10 +7,17 @@ function Cart() {
 
   useEffect(() => {
     // TODO 1: s'abonner aux ajouts au panier et mettre a jour le state items
+    const unsubscribe = eventBus.on('cart:add', (product) => {
+      setItems(prev => [...prev, { ...product, cartId: Date.now() + Math.random() }]);
+    });
+    return unsubscribe;
   }, []);
 
   useEffect(() => {
     // TODO 2: emettre un evenement quand le panier change
+    const data = { count: items.length, total: items.reduce((sum, item) => sum + item.price, 0) };
+    console.log('[EventBus] cart:updated', data);
+    eventBus.emit('cart:updated', data);
   }, [items]);
 
   const handleRemove = (cartId) => {
